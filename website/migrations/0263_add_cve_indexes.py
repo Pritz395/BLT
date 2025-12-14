bcb72fd37ed1f8979adc263fcc7565bfbf10a9a9
preetham <preethampujari395@gmail.com>
2025-12-14 19:35:52 +0530

    fix: Renumber CVE migration to 0263 to resolve collision

--- website/migrations/0263_add_cve_indexes.py
+++ website/migrations/0263_add_cve_indexes.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("website", "0260_add_username_to_slackbotactivity"),
+        ("website", "0262_securityincident_securityincidenthistory_and_more"),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
